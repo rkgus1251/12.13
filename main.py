567efd0d4d5101c8928043fba8c5decb3716b3cb
rlaalsrbaa <rlaalsrbaa@gmail.com>
2021-12-13 10:50:46 +0900

    1.1

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("안녕하세요"
